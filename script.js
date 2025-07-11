091038dead500d353ffa39abe8efd1c7c54e1558
Nha-Kboy <panharin502@gmail.com>
2025-07-11 13:00:56 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+const btn = document.getElementById('learnBtn');
+
+btn.addEventListener('click', () => {
+  // នៅពេលប៊ូតុងត្រូវបានចុច វានឹងបញ្ជូនអ្នកប្រើប្រាស់ទៅ video-page.html
+  window.location.href = "video-page.html";
+});
