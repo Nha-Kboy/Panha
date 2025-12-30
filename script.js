bd1b58d535b0849ff6a77492b215d96b300de95c
Nha-Kboy <panharin502@gmail.com>
2025-12-30 10:16:16 +0700

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-const btn = document.getElementById('learnBtn');
-
-btn.addEventListener('click', () => {
-  // នៅពេលប៊ូតុងត្រូវបានចុច វានឹងបញ្ជូនអ្នកប្រើប្រាស់ទៅ video-page.html
-  window.location.href = "video-page.html";
-});
